6084674b5fec4a672b212c4cf25bf0e5cfbb1693
bokeumjeon <bokeumjeon@gmail.com>
2024-01-28 12:55:41 -0500

    week 2

---
+++ bokeum is confused and trying/script.js
@@ -0,0 +1,34 @@
+function schedule() {
+    var date = new Date();
+    var time = date.toLocaleTimeString();
+    var today = date.getHours();
+  
+    //apply the time to the html element
+    $("#time").text(time);
+  
+    //check if opened, else closed
+    if (today >= 6 && today <= 10) {
+      //remove and add class to change the gradient of the bg
+      $("body").removeClass("closed");
+      $("body").addClass("open");
+  
+      //toggle between messages
+      $("#open-message").css("display", "block");
+      $("#closed-message").css("display", "none");
+    } else {
+      //remove and add class to change the gradient of the bg
+      $("body").removeClass("open");
+      $("body").addClass("closed");
+  
+      //toggle between messages
+      $("#open-message").css("display", "none");
+      $("#closed-message").css("display", "block");
+    }
+  }
+  //initalize function
+  schedule();
+  
+  setInterval(function() {
+    //check time every minute
+    schedule();
+  }, 60000);
